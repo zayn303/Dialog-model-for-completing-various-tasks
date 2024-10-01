7fc3b0e514b30ae90a361dd719ad9d0d020f7fb4
Andrii Kolomiiets <kolomiietsandrii1@gmail.com>
2024-10-01 17:52:10 +0000

    added clean_up_tokenization_spaces=False

--- models/summarization_model.py
+++ models/summarization_model.py
@@ -2,7 +2,9 @@ from transformers import pipeline
 
 class SummarizationModel:
     def __init__(self, model_name="facebook/bart-large-cnn"):
+        # Create the summarization pipeline
         self.model = pipeline("summarization", model=model_name)
 
     def summarize(self, text):
-        return self.model(text)
+        # Pass clean_up_tokenization_spaces=False to avoid the warning
+        return self.model(text, clean_up_tokenization_spaces=False)
